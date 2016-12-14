ef9553400df9a53a8a0fa9c2a7fa4c5e0fb12505
Rene Schmidt <rene+_gth@reneschmidt.de>
2016-12-15 00:05:11 +0100

    mkinitcpio and syslinux setup for encryption

--- bin/arch_base.sh
+++ bin/arch_base.sh
@@ -75,11 +75,11 @@ cp /usr/lib/syslinux/bios/libcom32.c32 /usr/lib/syslinux/bios/menu.c32 /usr/lib/
 
 # Setup /etc/mkinitcpio.conf; add "encrypt" and "lvm" hooks
 sed -i -- "s/^HOOKS=/#HOOKS=/g" /mnt/etc/mkinitcpio.conf
-echo 'HOOKS="base udev autodetect modconf block encrypt lvm filesystems keyboard fsck"' >> /mnt/etc/mkinitcpio.conf
+echo 'HOOKS="base udev autodetect modconf block encrypt lvm filesystems keyboard fsck"' >> /etc/mkinitcpio.conf
 SYSTEM_UUID=`blkid -s UUID -o value "${DISK_SYSTEM}"`
 
 #If you use encryption LUKS change the APPEND line to use your encrypted volume:
-echo "#APPEND root=/dev/mapper/SDOVG cryptdevice=UUID=${SYSTEM_UUID}:lvm rw" >> /mnt/boot/syslinux/syslinux.cfg
+echo "#APPEND root=/dev/mapper/SDOVG cryptdevice=UUID=${SYSTEM_UUID}:lvm rw" >> /boot/syslinux/syslinux.cfg
 
 # Finish
 echo "Done."
