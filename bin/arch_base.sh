a74ab5ec86291915bf3d0671d913eb778c47bc20
Rene Schmidt <rene+_gth@reneschmidt.de>
2016-12-17 23:07:32 +0100

    test pacstrap

--- bin/arch_base.sh
+++ bin/arch_base.sh
@@ -39,40 +39,39 @@ genfstab -U /mnt >> /mnt/etc/fstab
 
 # Chroot into new env
 echo "Chrooting..."
-arch-chroot /mnt
 
 # Install Intel microcode
 echo "Install Intel microcode..."
-pacman -S --noconfirm intel-ucode syslinux grub
+arch_chroot "pacman -S --noconfirm intel-ucode syslinux grub"
 
 # Update timezone and system time
 echo "Setting time and time zones..."
-ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
+ln -sf /mnt/usr/share/zoneinfo/Europe/Berlin /mnt/etc/localtime
 hwclock --systohc
 
 # Setup locales and keymap
 echo "Setup locales and keymap..."
-echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
-echo "de_DE@euro ISO-8859-15" >> /etc/locale.gen
-echo "de_DE.UTF-8 UTF-8" >> /etc/locale.gen
+echo "en_US.UTF-8 UTF-8" > /mnt/etc/locale.gen
+echo "de_DE@euro ISO-8859-15" >> /mnt/etc/locale.gen
+echo "de_DE.UTF-8 UTF-8" >> /mnt/etc/locale.gen
 
-echo "LANG=de_DE.UTF-8" > /etc/locale.conf
-echo "KEYMAP=de-latin1-nodeadkeys" > /etc/vconsole.conf
+echo "LANG=de_DE.UTF-8" > /mnt/etc/locale.conf
+echo "KEYMAP=de-latin1-nodeadkeys" > /mnt/etc/vconsole.conf
 
 # Basic network setup
 echo "Basic network setup..."
 HOSTNAME="sdotestsystem"
-echo "${HOSTNAME}" > /etc/hostname
-echo "127.0.0.1 localhost.localdomain localhost" > /etc/hosts
-echo "::1 localhost.localdomain localhost" >> /etc/hosts
-echo "127.0.1.1 ${HOSTNAME}.localdomain ${HOSTNAME}" >> /etc/hosts
+echo "${HOSTNAME}" >/mnt/etc/hostname
+echo "127.0.0.1 localhost.localdomain localhost" >/mnt/etc/hosts
+echo "::1 localhost.localdomain localhost" >>/mnt/etc/hosts
+echo "127.0.1.1 ${HOSTNAME}.localdomain ${HOSTNAME}" >>/mnt/etc/hosts
 
 # Install boot loader
 echo "Install boot loader..."
-mkdir /boot/syslinux
-extlinux --install /boot/syslinux
-cat /usr/lib/syslinux/bios/mbr.bin > "${DISK}"
-cp /usr/lib/syslinux/bios/libcom32.c32 /usr/lib/syslinux/bios/menu.c32 /usr/lib/syslinux/bios/libutil.c32 /boot/syslinux/
+mkdir /mnt/boot/syslinux
+arch_chroot "extlinux --install /mnt/boot/syslinux"
+cat /mnt/usr/lib/syslinux/bios/mbr.bin > "${DISK}"
+cp /mnt/usr/lib/syslinux/bios/libcom32.c32 /mnt/usr/lib/syslinux/bios/menu.c32 /mnt/usr/lib/syslinux/bios/libutil.c32 /mnt/boot/syslinux/
 
 #pacstrap / grub os-prober
 
@@ -80,16 +79,16 @@ cp /usr/lib/syslinux/bios/libcom32.c32 /usr/lib/syslinux/bios/menu.c32 /usr/lib/
 #    pacstrap / efibootmgr dosfstools
 #fi
 
-# Setup /etc/mkinitcpio.conf; add "encrypt" and "lvm" hooks
-sed -i -- "s/^HOOKS=/#HOOKS=/g" /etc/mkinitcpio.conf
-echo 'HOOKS="base udev autodetect modconf block encrypt lvm2 filesystems keyboard fsck"' >> /etc/mkinitcpio.conf
+# Setup/mnt/etc/mkinitcpio.conf; add "encrypt" and "lvm" hooks
+sed -i -- "s/^HOOKS=/#HOOKS=/g" /mnt/etc/mkinitcpio.conf
+echo 'HOOKS="base udev autodetect modconf block encrypt lvm2 filesystems keyboard fsck"' >>/mnt/etc/mkinitcpio.conf
 
 #If you use encryption LUKS change the APPEND line to use your encrypted volume:
 SYSTEM_UUID=`blkid -s UUID -o value "${DISK_SYSTEM}"`
 echo "Found UUID ${SYSTEM_UUID} for disk ${DISK_SYSTEM}!"
-#echo "#APPEND root=/dev/mapper/SDOVG-rootlv cryptdevice=UUID="${SYSTEM_UUID}":lvm rw" >> /boot/syslinux/syslinux.cfg
+#echo "#APPEND root=/dev/mapper/SDOVG-rootlv cryptdevice=UUID="${SYSTEM_UUID}":lvm rw" >> /mnt/boot/syslinux/syslinux.cfg
 
-mkinitcpio -p linux
+arch_chroot "mkinitcpio -p linux"
 
 # Finish
 echo "Done."
