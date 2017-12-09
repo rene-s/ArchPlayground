dd8b439fd430f6ac576f7c0fcae1048b98746a6c
René <rene@sdo.sh>
2017-12-09 11:47:14 +0100

    03: install cower and pacaur alongside yaourt

--- system/03_post_base_install.sh
+++ system/03_post_base_install.sh
@@ -11,3 +11,15 @@ wget https://aur.archlinux.org/cgit/aur.git/snapshot/yaourt.tar.gz -O - | tar xz
 
 cd /tmp/package-query;makepkg -si
 cd /tmp/yaourt;makepkg -si
+
+# Install pacaur
+sudo pacman -S expac yajl --noconfirm
+TMP_DIR=`mktemp -d`
+cd $TMP_DIR
+gpg --recv-keys --keyserver hkp://pgp.mit.edu 1EB2638FF56C0C53
+curl -o PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=cower
+makepkg -i PKGBUILD --noconfirm
+curl -o PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=pacaur
+makepkg -i PKGBUILD --noconfirm
+cd
+rm -rf $TMP_DIR
