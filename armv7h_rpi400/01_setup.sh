e2a7e2793104f620f784f3bd98e8e4aa5d9c8669
Rene <rene@sdo.sh>
2020-11-07 21:57:12 +0100

    feature: armv7h_rpi400: prepare sshd conf

--- armv7h_rpi400/01_setup.sh
+++ armv7h_rpi400/01_setup.sh
@@ -3,3 +3,7 @@
 mkdir -p ~/.ssh
 chown 0700 ~/.ssh
 curl -L "https://github.com/rene-s.keys" --output ~/.ssh/authorized_keys
+sed -i 's,^#PermitRootLogin .*,PermitRootLogin prohibit-password,g' /etc/makepkg.conf
+systemctl restart sshd
+
+#sed -i 's,#MAKEFLAGS="-j2",MAKEFLAGS="-j$(nproc)",g' /etc/makepkg.conf
