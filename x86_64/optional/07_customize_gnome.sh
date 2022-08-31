08c73eb3757a53e10d7408e1818f9aa67e0ed957
Rene <rene@sdo.sh>
2022-08-31 21:53:52 +0200

    feature: - fixed gnome customization script paths

--- x86_64/optional/07_customize_gnome.sh
+++ x86_64/optional/07_customize_gnome.sh
@@ -12,23 +12,23 @@ fi
 
 mkdir -p ~/Bilder
 
-. ./customize_gnome/009_keymap.sh
-. ./customize_gnome/005_wallpaper.sh
-. ./customize_gnome/006_gnome_settings.sh
-. ./customize_gnome/008_git.sh
+. "${DIR}/customize_gnome/009_keymap.sh"
+. "${DIR}/customize_gnome/005_wallpaper.sh"
+. "${DIR}/customize_gnome/006_gnome_settings.sh"
+. "${DIR}/customize_gnome/008_git.sh"
 
 # Misc stuff
 xdg-mime default org.gnome.Nautilus.desktop inode/directory # see https://wiki.archlinux.de/title/GNOME
 
-. ./customize_gnome/002_appindicator.sh
-. ./customize_gnome/004_seafile.sh
-. ./customize_gnome/003_misc_tools.sh
+. "${DIR}/customize_gnome/002_appindicator.sh"
+. "${DIR}/customize_gnome/004_seafile.sh"
+. "${DIR}/customize_gnome/003_misc_tools.sh"
 
 # Remove redundant packages
 pacman -Q vi 2>/dev/null && pacman -R --noconfirm vi || true
 pacman -Q vim 2>/dev/null && pacman -R --noconfirm vim || true
 
-. ./customize_gnome/001_tilix_style.sh
-. ./customize_gnome/007_avatar.sh
+. "${DIR}/customize_gnome/001_tilix_style.sh"
+. "${DIR}/customize_gnome/007_avatar.sh"
 
 echo "Done. Log out and log in again in order to actually see the changes made."
