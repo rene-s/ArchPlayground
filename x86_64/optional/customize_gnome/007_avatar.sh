58212fb41c341199f49f514616e6115b311f44f4
Rene Schmidt <rene@sdo.sh>
2025-03-08 21:23:21 +0100

    fixed usage of ImageMagick's convert tool

--- x86_64/optional/customize_gnome/007_avatar.sh
+++ x86_64/optional/customize_gnome/007_avatar.sh
@@ -8,6 +8,8 @@ bail_on_root
 sudo mkdir -p /var/lib/AccountsService/users || true
 sudo mkdir -p /usr/local/share/pixmaps/faces || true
 
+yay_inst_pkg imagemagick
+
 # Set avatar
 AVATAR="${USER}"
 SVG_FILE="/home/${USER}/Bilder/.${AVATAR}.svg"
@@ -18,10 +20,10 @@ curl -L "https://raw.githubusercontent.com/Schmidt-DevOps/Schmidt-DevOps-Static-
 
 if [[ -f "${SVG_FILE}" ]] && [[ -d /home/${USER}/Bilder/ ]]; then
   cd /home/"${USER}"/Bilder/ || exit
-  convert ".${AVATAR}.svg" ".${AVATAR}.png"
+  magick convert ".${AVATAR}.svg" ".${AVATAR}.png"
 
   # Scale avatar to 96px width, then crop 96x96px with 5px offset from the top. Save to non-home dir because GDM does not seem to like those.
-  sudo convert ".${AVATAR}.png" -resize 96x -crop 96x96+0+5 "${PNG_FILE}"
+  sudo magick convert ".${AVATAR}.png" -resize 96x -crop 96x96+0+5 "${PNG_FILE}"
 
   echo "[User]" | sudo tee "${USER_FILE}"
   echo "Language=de_DE.UTF-8" | sudo tee --append "${USER_FILE}"
