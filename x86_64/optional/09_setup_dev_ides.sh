64a136e238400c7d9949bf0fbd8457804ca50f01
Rene Schmidt <rene@sdo.sh>
2025-03-08 21:44:37 +0100

    disabled most IDEs

--- x86_64/optional/09_setup_dev_ides.sh
+++ x86_64/optional/09_setup_dev_ides.sh
@@ -7,15 +7,18 @@ cd "$DIR" || exit
 . "${DIR}/../lib/sharedfuncs.sh"
 bail_on_root
 
-yay_inst_pkg intellij-idea-ultimate-edition
-yay_inst_pkg intellij-idea-ultimate-edition-jre
+#yay_inst_pkg intellij-idea-ultimate-edition
+#yay_inst_pkg intellij-idea-ultimate-edition-jre
 yay_inst_pkg webstorm
 yay_inst_pkg webstorm-jre
-yay_inst_pkg phpstorm
-yay_inst_pkg phpstorm-jre
-yay_inst_pkg rubymine
-yay_inst_pkg goland
-yay_inst_pkg goland-jre
+#yay_inst_pkg phpstorm
+#yay_inst_pkg phpstorm-jre
+#yay_inst_pkg rubymine
+#yay_inst_pkg goland
+#yay_inst_pkg goland-jre
+yay_inst_pkg android-studio
+yay_inst_pkg intellij-idea-community-edition
+yay_inst_pkg vscodium
 
 yay_inst_pkg docker
 yay_inst_pkg jq
